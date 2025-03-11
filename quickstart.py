358a56a3eec281f4445ed6560640b77ec8a2610b
MaxSignal <89912994+MaxSignal@users.noreply.github.com>
2025-03-12 02:32:26 +0900

    Create quickstart.py

---
+++ quickstart.py
@@ -0,0 +1,5 @@
+import gdown
+
+url = 'https://drive.usercontent.google.com/download?id=1CA_1Awu-bewJKpDrILzWok_H_6cOkGDb'
+output = 'uHumans2_office_s1_00h.bag'
+gdown.download(url, output, quiet=False)
