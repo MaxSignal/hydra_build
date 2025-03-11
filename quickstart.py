b0fd487d2a144f595e67e2a1a32820e9161fe56c
MaxSignal <89912994+MaxSignal@users.noreply.github.com>
2025-03-12 02:48:51 +0900

    Update and rename quickstart.py to quickstart.sh

--- quickstart.py
+++
@@ -1,5 +0,0 @@
-import gdown
-
-url = 'https://drive.google.com/uc?export=download&id=1CA_1Awu-bewJKpDrILzWok_H_6cOkGDb'
-output = 'uHumans2_office_s1_00h.bag'
-gdown.download(url, output, quiet=False)
---
+++ quickstart.sh
@@ -0,0 +1,2 @@
+gdown 'https://drive.google.com/uc?export=download&id=1CA_1Awu-bewJKpDrILzWok_H_6cOkGDb' -O 'uHumans2_office_s1_00h.bag'
+rosbag decompress uHumans2_office_s1_00h.bag
